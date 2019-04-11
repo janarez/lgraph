c72672428126251dfc50b9031005fa309fa8a241
Jana Rezabkova <jana.rezabkova@datamole.cz>
2019-04-11 11:07:18 +0200

    Implementing the matrix idea in progress

--- P1/VertexPermuter.cpp
+++ P1/VertexPermuter.cpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <numeric>
+#include <algorithm>
 
 bool VertexPermuter::permuteVertices(void)
 {
@@ -41,12 +42,128 @@ bool VertexPermuter::createGraph(size_t offset, size_t n)
 		}
 		
 		// here all the matrix magic will be done;
-		p.clear();
+		p = nextPermutation(n, p);
 
 	} while (!p.empty());
 	
 	return false;
 }
+
+std::vector<size_t>& VertexPermuter::nextPermutation(size_t n, std::vector<size_t>& p)
+{
+	std::vector<size_t> incorrect;
+
+	for (size_t A = a; A < n; ++A)
+	{
+		for (size_t C = A; C < n; ++C)
+		{
+			if (matrix[A][C])
+			{
+				incorrect = findAlWrongInMatrix(A, C, n);
+
+				// invalid situations
+				if (!incorrect.empty())
+				{
+					// a will be set by this function as well
+					return handleInvalidInMatrix(incorrect, C);
+				}
+			}
+		}
+	}
+
+	// no invalid situations
+	if (incorrect.empty())
+	{
+		if (std::next_permutation(p.begin(), p.end()))
+		{
+			// this function also sets a
+			// this should be able to change the first vertex as well
+			permuteMatrix(p);
+			return p;
+		}
+		else
+		{
+			// returning empty vector = solution not found
+			return incorrect;
+		}
+	}
+}
+
+std::vector<size_t>& VertexPermuter::findAlWrongInMatrix(size_t A, size_t C, size_t n)
+{
+	std::vector<size_t> incorrect;
+
+	for (size_t B = A+1; B < C; ++B)
+	{
+		for (size_t D = C+1; D < n; ++D)
+		{
+			if (matrix[B][D] && !matrix[B][C] && !matrix[C][D] && !matrix[A][B])
+			{
+				size_t result = switchBorD(A, B, D, C);
+				if (result)
+					incorrect.push_back(result);
+				else
+				{
+					throw std::exception("Cannot be solved");
+				}
+			}
+		}
+	}
+	return incorrect;
+}
+
+size_t VertexPermuter::switchBorD(size_t A, size_t B, size_t D, size_t C)
+{
+	auto b = matrix[B].begin();
+	auto d = matrix[D].begin();
+
+	size_t bAB = std::count(b + A + 1, b + B, 1);
+	size_t bBC = std::count(b + B + 1, b + C, 1);
+	size_t bCD = std::count(b, matrix[B].end(), 1) - bAB - bBC - 1;
+
+
+	size_t dAB = std::count(d + A + 1, b + B, 1);
+	size_t dBC = std::count(d + B + 1, b + C, 1);
+	size_t dCD = std::count(d, matrix[D].end(), 1) - dAB - dBC;
+
+	if (inB > 0)
+	{
+		if (outB > 0)
+			// here C must be moved before first neighbor of B
+			return 0;
+		else if (outD == 0)
+			return D;
+		else
+			return 0;
+	}
+	else if (outB > 0)
+	{
+		if (inD == 0)
+			return B;
+		else
+			return 0;
+	}
+	else
+	{
+		if (outD > 0)
+			return B;
+		else
+			return D;
+	}
+}
+
+void VertexPermuter::permuteMatrix(std::vector<size_t>& p)
+{
+	return;
+}
+
+std::vector<size_t>& VertexPermuter::handleInvalidInMatrix(std::vector<size_t>& incorrect, size_t C)
+{
+	
+	return permutation;
+}
+
+
 
 void VertexPermuter::setMatrix(size_t start, size_t size)
 {
--- P1/VertexPermuter.h
+++ P1/VertexPermuter.h
@@ -14,20 +14,46 @@ class VertexPermuter
 	// adjacency matrix used to cut down # of possible permutations of each component
 	std::vector<std::vector<size_t>> matrix;
 
+	// component sizes
 	std::vector<size_t> comp_size;
 
+	// total number of vertices
 	size_t size_per;
+	// # of components
 	size_t size_comp;
 
+	// variable for matrix handling
+	size_t a = 1;
+
+	// loops through permutations
 	bool createGraph(size_t offset, size_t n);
+
+	// renames and truncates neighbors for a component
 	std::map<size_t, std::set<size_t>>& prepareNeighbors(size_t start, size_t n, std::vector<size_t>& perm);
 
+	// sets matrix for a given components - at the beggining
 	void setMatrix(size_t start, size_t size);
 
+	// fills permutation with correct permutation of one component
 	void fillPermutation(size_t offset, std::vector<size_t>& perm);
+	// finds next possible permutation to feed igraph
+	std::vector<size_t>& nextPermutation(size_t n, std::vector<size_t>& p);
+	// returns all BD edges that go against solvable matrix
+	std::vector<size_t>& findAlWrongInMatrix(size_t A, size_t C, size_t n);
+
+	// permutes matrix if no invalid situation is found, so that it corresponds to current permutation
+	void permuteMatrix(std::vector<size_t>& p);
+	
+	// puts matrix into correct state up with regards to a
+	std::vector<size_t>& handleInvalidInMatrix(std::vector<size_t>& incorrect, size_t C);
+
+	// decides whether the new matrix should have ABDC or ACBD
+	size_t switchBorD(size_t A, size_t B, size_t D, size_t C);
+
 
 public:
-
+	
+	// constructed with map of neighbors stacked by components and component sizes
 	VertexPermuter(std::map<size_t, std::set<size_t>> n, std::vector<size_t> c)
 	{
 		neighbors = n;
@@ -39,7 +65,7 @@ public:
 		permutation.resize(size_per + 1);
 	}
 
-	// public so that igraph can be set when solution is found in gl
+	// resulting permutation; public so that igraph can be set when solution is found in gl
 	std::vector<size_t> permutation;
 
 	// represents functionality of the whole class
